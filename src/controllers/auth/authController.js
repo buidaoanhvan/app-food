3146ee6af112ccbf9b5c0f339f68f601e654b597
Bùi Đào Anh Văn <buidaoanhvan1997@gmail.com>
2022-11-25 18:15:53 +0700

    Đang làm phần tạo đăng ký tài khoản

--- src/controllers/auth/authController.js
+++ src/controllers/auth/authController.js
@@ -1,13 +1,20 @@
 const sequelize = require('../../models')
 const initModels = require('../../models/init-models')
-const models = initModels(sequelize);
+const models = initModels(sequelize)
+const bcrypt = require("bcrypt")
 
 const authLogin = (req, res) => {
-    console.log(models)
+    res.send('login')
 }
 
-const authRegister = (req, res) => {
-    console.log(models)
+const authRegister = async (req, res) => {
+
+    const { password } = req.body
+
+    const passwordHash = await bcrypt.hash(password, 10)
+    console.log(passwordHash)
+
+    res.send('anhvan')
 }
 
 module.exports = { authLogin, authRegister }
